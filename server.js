7d5bc33aea3bb193d1e7fc28a1d762ae1ad4eacf
mukkadanlal <mukkadanlal@gmail.com>
2016-09-25 20:37:58 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.Send(createTemplate(articleOne));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
