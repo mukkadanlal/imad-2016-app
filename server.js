578276dce5b6428358c2402fc5efa48637f39e1f
mukkadanlal <mukkadanlal@gmail.com>
2016-09-25 21:04:41 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articleOne ={
+var article ={
+    article-one:{
     title:'Article One | Lal George',
     heading :'Article One',
-    date:'Sep 25,2017',
+    date:'Sep 20,2017',
     content:`<p>
                 The example companies, organizations, products, domain names, email addresses, logos, people, places, and events depicted herein are fictitious. No association with any real company, organization, product, domain name, email address, logo, person, place, or event is intended or should be inferred.
                     </p>
@@ -17,7 +18,30 @@ var articleOne ={
                     <p>
                 If you are new to ASP.NET Web Forms but have familiarity with programming concepts, you have the right tutorial. If you are already familiar with ASP.NET Web Forms, you can benefit from this tutorial series by the new features available in ASP.NET 4.5. If you are unfamiliar with programming concepts and ASP.NET Web Forms, see the additional tutorials provided in the Web Forms Getting Started section on the ASP.NET Web site.
                 </p>`
-    };
+    },
+    article-two :{
+    title:'Article Two | Lal George',
+    heading :'Article Two',
+    date:'Sep 25,2017',
+    content:`<p>
+                The example companies, organizations, products, domain names, email addresses, logos, people, places, and events depicted herein are fictitious. No association with any real company, organization, product, domain name, email address, logo, person, place, or event is intended or should be inferred.
+            </p>
+            <p>
+                This book expresses the author's views and opinions. The information contained in this book is provided without any express, statutory, or implied warranties. Neither the authors, Microsoft Corporation, nor its resellers, or distributors will be held liable for any damages caused or alleged to be caused either directly or indirectly by this book.
+            </p>`
+    },
+    article-three:{
+    title:'Article Three | Lal George',
+    heading :'Article Three',
+    date:'Sep 30,2017',
+    content:`<p>
+              The example companies, organizations, products, domain names, email addresses, logos, people, places, and events depicted herein are fictitious. No association with any real company, organization, product, domain name, email address, logo, person, place, or event is intended or should be inferred.
+              </p>
+              <p>
+              If you are new to ASP.NET Web Forms but have familiarity with programming concepts, you have the right tutorial. If you are already familiar with ASP.NET Web Forms, you can benefit from this tutorial series by the new features available in ASP.NET 4.5. If you are unfamiliar with programming concepts and ASP.NET Web Forms, see the additional tutorials provided in the Web Forms Getting Started section on the ASP.NET Web site.
+              </p>`
+    },
+};
 function createTemplate (data){
     var title = data.title;
     var heading = data.heading;
@@ -46,8 +70,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-   res.send(createTemplate(articleOne));
+app.get('/:articleName',function(req,res){
+   res.send(createTemplate(article[articleName]);
 });
 
 app.get('/article-two',function(req,res){
