479e89b3d70e1b7ffbe8851af003ba56d46e805e
mukkadanlal <mukkadanlal@gmail.com>
2016-09-28 12:54:34 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
 var element = document.getElementById('test');
-element.innerHtml = "New value replacedwhile loading the document";
+element.innerHTML = "New value replacedwhile loading the document";
