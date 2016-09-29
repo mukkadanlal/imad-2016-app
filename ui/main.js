7f3b466a4698b0960be62f088d4e66a7ec037684
mukkadanlal <mukkadanlal@gmail.com>
2016-09-29 10:06:06 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ element.innerHTML="New value loaded while loading the document";*/
 var button = document.getElementById("counter");
 button.onClick(function(){
     var request = new XMLHttpRequest();
-    request.open('GET','http://mukkadanlal.imad.hasura.app.io/counter',true);
+    request.open('GET','http://mukkadanlal.imad.hasura-app.io/counter',true);
     request.send();
     request.onreadystatechange = function (){
         if (request.readyState=== XMLHttpRequest.Done){
