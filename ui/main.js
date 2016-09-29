89f0e0d1c155f472c6007e8b8f57076f3e187cf3
mukkadanlal <mukkadanlal@gmail.com>
2016-09-29 13:03:09 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ var name = nameInput.Value;
 var submit = document.getElementById("submit-btn");
 submit.onclick = function (){
     
-  var names =[name1,name2,name3,name4]  ;
+  var names =['name1','name2','name3','name4']  ;
   var list="";
   for (var i=0;i<names.length;i++)
   {
