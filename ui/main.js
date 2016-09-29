2716f03ff46f5e2461dda1c2f4f883ed33157024
mukkadanlal <mukkadanlal@gmail.com>
2016-09-29 19:02:40 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,12 +22,12 @@ submit.onclick = function (){
     var request = new XMLHttpRequest();
     var nameInput = document.getElementById("name");
     var name = nameInput.Value;
-    request.open('GET','http://mukkadanlal.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET','http://mukkadanlal.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send();
     request.onreadystatechange = function (){
         if (request.readyState=== XMLHttpRequest.DONE){
             if(request.status===200){
-            var names =rer.responseText;
+            var names =req.responseText;
             names = JSON.parse(names);
               var list=``;
               for (var i=0;i<names.length;i++)
