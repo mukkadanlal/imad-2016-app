a6de01bcb7224340890ca1fea18058cd5cee4c37
mukkadanlal <mukkadanlal@gmail.com>
2016-09-29 19:11:06 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function (){
         }
     };  
     var nameInput = document.getElementById("name");
-    var name = nameInput.Value;
+    var name = nameInput.value;
     request.open('GET','http://mukkadanlal.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
   };
